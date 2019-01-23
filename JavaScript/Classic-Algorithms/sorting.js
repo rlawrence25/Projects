3bff064eac1cf14de10d88450d5c186593568395
Raquel Lawrence <raquel.lawrence@anthem.com>
2019-01-23 10:31:50 -0500

    added initial languages

---
+++ JavaScript/Classic-Algorithms/sorting.js
@@ -0,0 +1,36 @@
+/**
+ * A divide and conquer algorithms. Divides a given array in half, call itself 
+ * for the two halves and then mergers the two sorted halves. There is a separte merge
+ * function `merge(arr, l, m, r)` -> `arr[l...m]` and `arr[m + 1...r]`
+ */
+
+const merge = (arr) => {
+
+}
+
+const mergesort = () => {
+
+}
+
+
+/**
+ * 
+ */
+const quicksort = () => {
+
+}
+
+
+/**
+ * 
+ */
+const insertionsort = () => {
+
+}
+
+
+module.exports = {
+    mergesort,
+    quicksort,
+    insertionsort,
+}
